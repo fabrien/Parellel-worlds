3a06b4277b15abb84ea62af52eaa39b3cbc7b036
Adrien Fabre <adriofab@hotmail.com>
2021-03-19 16:29:48 +0100

    Added user input class

--- Assets/UserInput.cs
+++ Assets/UserInput.cs
@@ -1,4 +1,58 @@
-﻿public class UserInput
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class UserInput : MonoBehaviour
 {
+    public Vector2Int input;
+    
+    public void Update()
+    {
+        var horizontal = Horizontal.None;
+        var vertical = Vertical.None;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            vertical = Vertical.Down;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            vertical = Vertical.Up;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            horizontal = Horizontal.Right;
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            horizontal = Horizontal.Left;
         
+        input = InputToVector(horizontal, vertical);
+    }
+
+    private static Vector2Int InputToVector(Horizontal horizontal, Vertical vertical)
+    {
+        var x = horizontal switch
+        {
+            Horizontal.Left => -1,
+            Horizontal.Right => 1,
+            _ => 0
+        };
+        var y = vertical switch
+        {
+            Vertical.Up => 1,
+            Vertical.Down => -1,
+            _ => 0
+        };
+        return new Vector2Int(x, y);
+    }
+}
+
+
+public enum Horizontal
+{
+    None,
+    Right,
+    Left
+}
+
+public enum Vertical
+{
+    None,
+    Up,
+    Down
 }
